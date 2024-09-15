92f7a2dae5dc4e2774613b36f7e7ceb8c5bdad96
Siva Senthil kumar <131667343+sivatharanish@users.noreply.github.com>
2024-09-15 14:46:10 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.camera-feed').forEach(function(video) {
+    video.addEventListener('click', function() {
+        if (this.requestFullscreen) {
+            this.requestFullscreen();
+        } else if (this.mozRequestFullScreen) { // Firefox
+            this.mozRequestFullScreen();
+        } else if (this.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            this.webkitRequestFullscreen();
+        } else if (this.msRequestFullscreen) { // IE/Edge
+            this.msRequestFullscreen();
+        }
+    });
+});
